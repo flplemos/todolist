7fcad5140d0a79935f77577fc42833bb1bcd9c67
flplemos <139652554+flplemos@users.noreply.github.com>
2023-10-10 12:50:18 -0300

    Initial commit

---
+++ src/main/java/br/com/flplemos/todolist/user/UserController.java
@@ -0,0 +1,52 @@
+package br.com.flplemos.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+/**
+ * Modificador
+ * Public
+ * Private
+ * Protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+
+    @Autowired
+    private IUserRepository userRepository;
+    
+    /**
+     * String (texto)
+     * Integer (int) Números inteiros
+     * Double (double) Números 0.0000
+     * Float (float) Números 0.000
+     * char (A C ) 
+     * Date (data)
+     * void
+     */
+    /**
+     * Body
+     */
+
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+}
