717b5d8b4ed9f0a6b2ad8d1ddcf2f2d402962f5b
flplemos <139652554+flplemos@users.noreply.github.com>
2023-10-11 11:19:26 -0300

    Atualizações de autenticação
    Realizando atualizações para autenticação de usuário.

--- src/main/java/br/com/flplemos/todolist/user/UserController.java
+++ src/main/java/br/com/flplemos/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 
 
 /**
@@ -45,6 +47,11 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashred = BCrypt.withDefaults()
+        .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
