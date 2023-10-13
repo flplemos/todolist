9561234f41ab2a60ccdba6d8325b153ce5ab2c4e
flplemos <139652554+flplemos@users.noreply.github.com>
2023-10-13 16:01:37 -0300

    atualizações de erros

--- src/main/java/br/com/flplemos/todolist/task/TaskController.java
+++ src/main/java/br/com/flplemos/todolist/task/TaskController.java
@@ -54,16 +54,26 @@ public class TaskController {
     }
 
     @PutMapping ("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
        
 
 
         var task = this.taskRepository.findById(id).orElse(null);
 
+        if(task == null) {
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario não tem permissão para alterar essa tarefa");
+        }
+
         Utils.copyNonNullProperties(taskModel, task);
 
-    
-        return this.taskRepository.save(task);
+        var taskUptdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUptdated);
     }
 
 }
